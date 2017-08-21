7e3e06f86cf859644fe771d7cfd79f5884f0f928
dariki1 <james_atkins97@hotmail.com>
2017-08-21 17:46:15 +1000

    004 complete
    004 is complete, very brute force approach

--- Project Eular/004; Largest palindrom product/script.js
+++ Project Eular/004; Largest palindrom product/script.js
@@ -3,3 +3,45 @@ A palindromic number reads the same both ways. The largest palindrome made from 
 
 Find the largest palindrome made from the product of two 3-digit numbers.
  */
+
+function isPalindrome(num) {
+    //Convert num to string
+    let sNum = num+"";
+    //Compare string to reversed string (convert to array of characters, reverse, then join to string)
+    return sNum == sNum.split("").reverse().join("");
+}
+
+function start() {
+    /*let i = 999;
+    let e = 999;
+
+    do {
+        if (isPalindrome(e*i)) {
+            output(i*e + " is " + e + " * " + i);
+            return;
+        }
+
+        if ((i-1)*e < i*(e-1) ) {
+            e--;
+        } else {
+            i--;
+        }
+    } while (true);*/
+
+    
+    let l = 0;
+
+    for (let i = 999; i > 99; i--) {
+        for (let e = 999; e > 99; e--) {
+            if (isPalindrome(e*i)) {
+                if (e*i > l) {
+                    l = e*i;
+                    output("New largest; " + l + " is " + i + " * " + e);
+                }
+            }
+        }
+    }
+    
+}
+
+start();
