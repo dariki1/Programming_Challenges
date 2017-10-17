ba3a5e7ea402be581ed7f6efc512f7da9af36708
dariki1 <james_atkins97@hotmail.com>
2017-10-17 22:05:37 +1100

    Started a self made challenge
    Every number can be expressed by only one combination (only using + and
    -) of unique (only occurs once in the expression at most) powers of
    three in a combination of adding and subtracting

---
+++ SelfMade/001;PowerThreeAddition/PowerThreeAdition.js
@@ -0,0 +1,6 @@
+/*
+Every number can be expressed by only one combination (only using + and -)
+of unique (only occurs once in the expression at most)
+powers of three in a combination of adding and subtracting.
+Create a program that outputs the appropriate expression for any given number
+*/
