7e32f12a6021466c155c9cf63451f29830094044
dariki1 <james_atkins97@hotmail.com>
2017-10-18 14:44:21 +1100

    Finished and redifined input coniditions

--- SelfMade/001;PowerThreeAddition/PowerThreeAdition.js
+++ SelfMade/001;PowerThreeAddition/PowerThreeAdition.js
@@ -1,13 +1,14 @@
 //TODO Fix getPow rounding errors from floating point
 
 /*
-Every number can be expressed by only one combination (only using + and -)
+Every positive whole number can be expressed by only one combination (only using + and -)
 of unique (only occurs once in the expression at most)
 powers of three in a combination of adding and subtracting.
-Create a program that outputs the appropriate expression for any given number
+Create a program that outputs the appropriate expression for any given positive whole number
 */
 
 /*
+*0 = 0
 *1 = 3^0
 *2 = 3^1-3^0
 3 = 3^1
@@ -25,14 +26,36 @@ Create a program that outputs the appropriate expression for any given number
 */
 
 function PTA(num) {
-	for (let i = -14; i < 15; i++) {
-		console.log(i+";"+getPow(i));
+	for (let i = 0; i <= 121; i++) {
+		console.log(getExpression(i));
 	}
 }
 
+function getExpression(num) {
+	let ret = num + " = ";
+	while (num != 0) {
+		let tPow = getHPow(num);
+		if (num < 0) {
+			ret += "- ";
+		} else {
+			ret += "+ ";
+		}
+		ret += "3^" + tPow + " ";
+		num += Math.pow(3,tPow)*Math.sign(num)*-1;
+	}
+	return ret;
+}
+
 //Returns the highest power of three needed for num to be expressed
-function getPow(num) {
-	return Math.ceil(Math.log(2*num+1)/Math.log(3))-1;
+function getHPow(num) {
+	num = Math.abs(num);
+	let ret = Math.log(2*num+1)/Math.log(3);
+	//This 'fixes' the rounding error caused by floating point rounding errors
+	ret *= 1000;
+	ret = Math.floor(ret);
+	ret /= 1000;
+	
+	return Math.ceil(ret)-1;
 }
 
 PTA();
